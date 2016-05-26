6a39657ad31940dac3bd1c6e04d95bc43494e036
Kyle Hamburg <kylelhamburg@gmail.com>
2016-05-26 14:34:16 -0600

    Finished the exercises

--- ccPractice.js
+++ ccPractice.js
@@ -3,21 +3,46 @@ var add, subtract, multiply, divide, calculate, reverseOrder, userHelp, someFunc
 
 
 //1. We are going to create a simple calculator. Go ahead and create four functions called add, subtract, multiply, and divide that take in two numbers that add, subtract, multiply, and divide the two inputted values. In each function return the result.
-
+function add(a,b){
+  return a+b;
+}
+function subtract(a,b){
+  return a-b;
+}
+function multiply(a,b){
+  return a*b;
+}
+function divide(a,b){
+  return a/b;
+}
 // Next, you will want to create a function called calculate that takes in two numbers and a callback function. Return the result of the callback with the two numbers as its arguments.
-
+function calculate(a,b,callback){
+  return callback(a,b);
+}
 
 // 2. Create a function called reverseOrder that takes in a string and reverses the order of the characters. Return the result.
-
+function reverseOrder(string){
+  var newString="";
+  for (var i = 0; i < string.length; i++){
+    newString+= string[string.length-i-1];
+  }
+  return newString;
+}
 // Create a function called userHelp that takes in a callback function and a quote. Invoke the callback function using the quote as an argument. Return the result of the callback. This should take a user defined quote and reverse the order of the quote.
-
+function userHelp(callback,quote){
+  return callback(quote);
+}
 
 // Closures
 
 // 1. Create a function called someFunc that takes in a number and returns the definition of another anonymous function. That anonymous function will take in a second number and will return the sum of both numbers.
-
+function someFunc(num){
+  return function(num1){
+    return num + num1;
+  }
+}
 // Invoke the someFunc function, and pass in a number value (Let's use number 9) as the argument. Assign the response to another variable called otherFunc.
-
+var otherFunc = someFunc(1);
 // When you are done, we can invoke the otherFunc function and pass in another number value as its argument. And we can do this as often as we like. Go ahead and uncomment out the following invocations.
 
 // otherFunc(10) // 19
@@ -31,17 +56,23 @@ var add, subtract, multiply, divide, calculate, reverseOrder, userHelp, someFunc
 
 
 // 2. Next, you will create a counter closure function that can be called at any point and will retain the number's new value even after being called over and over again.
-
+function countSet(){
+  var count=0;
+  return function(){
+    count++;
+    return count;
+  }
+}
 // Create a function called countSet, no parameters needed, and set a new variable (call it countup) to equal 0. Have the function return an anonymous function that returns the countup variable after being incremented.
-
+var counter = countSet();
 // Assign the invocation of the countSet function to a new variable. Let's call it counter.
 
 // Then whenever you need to count up, you can invoke the counter function. Go ahead and uncomment out the following invocations.
 
-// counter()//1
-// counter()//2
-// counter()//3
-// counter()//4
+ counter()//1
+ counter()//2
+ counter()//3
+ counter()//4
 
 module.exports = {
   add: add,
